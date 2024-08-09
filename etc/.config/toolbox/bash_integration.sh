05802a0359b1085e41f6ad7720eff100ece3d97d
carnivuth <matti200042@gmail.com>
2024-08-09 23:50:10 +0200

    added some aliases

--- etc/.config/toolbox/bash_integration.sh
+++ etc/.config/toolbox/bash_integration.sh
@@ -3,9 +3,19 @@ export PATH="$HOME/.local/bin:$PATH"
 export FZF_DEFAULT_OPTS='--cycle --bind "tab:toggle-up,btab:toggle-down"'
 alias j='project.sh'
 alias tls='tmux ls'
+alias tk='tmux kill-session -t'
 alias si='store.sh install'
 alias sr='store.sh remove'
 alias n='notify.sh'
 alias nh='notify.sh -t homelab'
 alias nw='notify.sh -t work'
 alias nr='notify.sh -t random'
+
+#ls aliases
+alias ls='ls --color=auto '
+alias ll='ls --color=auto -pl'
+alias la='ls --color=auto -pa'
+alias lla='ls --color=auto -pla'
+
+# clear
+alias c='clear'
