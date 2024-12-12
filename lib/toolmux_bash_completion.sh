a85fab8239ea6120139751f0f25076fa55f96065
carnivuth <matti200042@gmail.com>
2024-12-12 13:00:27 +0100

    added guard on tmux process not running

--- lib/toolmux_bash_completion.sh
+++ lib/toolmux_bash_completion.sh
@@ -3,7 +3,9 @@
 _tmux_complete_session() {
   local IFS=$'\n'
   local cur=${COMP_WORDS[COMP_CWORD]}
+  if ps aux | awk -F' ' '{print $11}' | grep -qe tmux; then
   COMPREPLY=( ${COMPREPLY[@]:-} $(compgen -W "$(tmux -q list-sessions | cut -f 1 -d ':')" -- "${cur}") )
+  fi
 }
 complete -A directory -F _tmux_complete_session tmx
 complete -A directory -F _tmux_complete_session toolmux.sh
