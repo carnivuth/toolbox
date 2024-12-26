a70dc700f96e3bc70753c46af6dde986846a8ab3
carnivuth <matti200042@gmail.com>
2024-12-26 11:57:53 +0100

    added html support

--- etc/nvim/lua/plugins/mason-lspconfig.lua
+++ etc/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,6 @@
 return {
   "williamboman/mason-lspconfig.nvim",
   opts={
-    ensure_installed= {"vimls", "ansiblels", "gopls", "pyright", "bashls", "terraformls", "docker_compose_language_service", "dockerls","eslint","lua_ls","yamlls"},
+    ensure_installed= {"vimls", "ansiblels", "gopls", "pyright", "bashls", "terraformls", "docker_compose_language_service", "dockerls", "eslint", "html", "lua_ls", "yamlls" },
   }
 }
