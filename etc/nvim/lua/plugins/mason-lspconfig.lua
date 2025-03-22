89e0bf2424fb4f0cd7984cb6bd3de81644c58461
carnivuth <matti200042@gmail.com>
2025-03-22 15:36:40 +0100

    minor fix

--- etc/nvim/lua/plugins/mason-lspconfig.lua
+++ etc/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,6 +1,19 @@
 return {
   "williamboman/mason-lspconfig.nvim",
   opts={
-    ensure_installed= {"vimls", "ansiblels", "gopls", "pyright", "bashls", "terraformls", "docker_compose_language_service", "dockerls", "eslint", "html", "lua_ls", "yamlls" },
+    ensure_installed= {
+      "vimls",
+      "ansiblels",
+      "gopls",
+      "pyright",
+      "bashls",
+      "terraformls",
+      "docker_compose_language_service",
+      "dockerls",
+      "eslint",
+      "html",
+      "lua_ls",
+      "yamlls",
+    },
   }
 }
