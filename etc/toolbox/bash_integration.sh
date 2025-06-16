2b6b27044e408867ece59daa8ded083ee2ecd35b
Matteo Longhi <mlonghi@labs.it>
2025-06-16 12:19:56 +0200

    removed minimal environment

--- etc/toolbox/bash_integration.sh
+++ etc/toolbox/bash_integration.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
-source "$HOME/.local/lib/minimal_env.sh"
 source "$HOME/.local/lib/toolmux_bash_completion.sh"
 
 # add toolbox bins to path
 export PATH="$HOME/.local/bin:$PATH"
 
 # set manpager
-if ! minimal_env; then export MANPAGER="nvim +Man! ";else export MANPAGER="vim -M +MANPAGER - "; fi
+export MANPAGER="nvim +Man! "
 
 # set editor variable
-if ! minimal_env; then export EDITOR="nvim";else export EDITOR="vim"; fi
+export EDITOR="nvim"
 
 # set fzf default command
 export FZF_DEFAULT_COMMAND='rg "" -l '
 
 # set fzf default options
-if minimal_env; then
-export FZF_DEFAULT_OPTS=" \
---cycle --bind 'tab:toggle-up,btab:toggle-down'"
-else
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 --color=selected-bg:#45475a \
 --cycle --bind 'tab:toggle-up,btab:toggle-down'"
-fi
 
 alias s='sudo'
 
 # tmux aliases
 alias tmls='tmux ls'
-alias tmm='toolmux.sh $HOME'
 alias tmk='tmux kill-session -t'
 alias tmks='tmux kill-server'
 # bash script to open tmux in a default layout
@@ -71,4 +64,4 @@ alias ssh='TERM=xterm-256color ssh'
 alias sss='sssh.sh'
 
 # starship integration
-if ! minimal_env; then eval "$(starship init bash)"; fi
+eval "$(starship init bash)"
