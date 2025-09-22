f7d3881aa06104c2f19dc6296f4de05d99159642
Matteo Longhi <mlonghi@labs.it>
2025-09-22 16:43:39 +0200

    fixed lsp configuration

--- etc/nvim/lua/lsp.lua
+++ etc/nvim/lua/lsp.lua
@@ -1,83 +1,91 @@
 -- BASH
-require("lspconfig").bashls.setup{}
-require("lspconfig").yamlls.setup{
-  filetypes ={"yaml","ansible","docker-compose"};
-}
+local lsps = {
 
--- DOCKER COMPOSE
-require("lspconfig").docker_compose_language_service.setup{
-  filetypes ={"docker-compose"};
-}
-
--- DOCKER
-require("lspconfig").dockerls.setup{}
-
--- TERRAFORM
-require("lspconfig").terraformls.setup{}
-
--- ANSIBLE
-require('lspconfig').ansiblels.setup{
-  filetypes ={"ansible"};
-  settings ={
-    ansible = {
-      ansible = {
-        path = vim.fn.getcwd() .. "/env/bin/ansible"
-      },
-      executionEnvironment = {
-        enabled = false
-      },
-      python = {
-        interpreterPath = vim.fn.getcwd() .. "/env/bin/python3"
-      },
-      validation = {
-        enabled = true,
-        lint = {
-          enabled = true,
-          path = vim.fn.getcwd() .. "/env/bin/ansible-lint",
+    { "bashls" },
+    { "html" },
+    { "terraformls" },
+    { "gopls" },
+    { "eslint" },
+    { "dockerls" },
+    {
+      "yamlls",{
+        filetypes ={"yaml","ansible","docker-compose"};
+      }
+    },
+    {
+      "docker_compose_language_service",{
+        filetypes ={"docker-compose"}
+      }
+    },
+    {
+      "lua_ls",{
+        settings = {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+              version = 'LuaJIT',
+              -- Setup your lua path
+              path = runtime_path,
+            },
+            diagnostics = {
+              globals = { 'vim' },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file('', true),
+              checkThirdParty = false,
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = { enable = false },
+          },
+        }
+      }
+    },
+    {
+      "ansiblels",{
+        filetypes ={"ansible"};
+        settings ={
+          ansible = {
+            ansible = {
+              path = vim.fn.getcwd() .. "/env/bin/ansible"
+            },
+            executionEnvironment = {
+              enabled = false
+            },
+            python = {
+              interpreterPath = vim.fn.getcwd() .. "/env/bin/python3"
+            },
+            validation = {
+              enabled = true,
+              lint = {
+                enabled = true,
+                path = vim.fn.getcwd() .. "/env/bin/ansible-lint",
+              }
+            }
+          }
+        }
+      }
+    },
+  {
+    "pyright",{
+      settings = {
+        python = {
+          pythonpath  = vim.fn.getcwd() .. "/env/bin/python3"
         }
       }
     }
-  }
+  },
 }
 
--- PYTHON
-require("lspconfig").pyright.setup{
-  settings = {
-    python = {
-      pythonPath  = vim.fn.getcwd() .. "/env/bin/python3"
-    }
-  }
-}
+for _, lsp in pairs(lsps) do
 
--- GO
-require("lspconfig").gopls.setup{}
+    local name, config = lsp[1], lsp[2]
 
--- JAVASCRIPT
-require("lspconfig").eslint.setup{}
+    vim.lsp.enable(name)
 
--- HTML
-require("lspconfig").html.setup{}
+    if config then
 
--- LUA
-require'lspconfig'.lua_ls.setup{
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        globals = { 'vim' },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file('', true),
-        checkThirdParty = false,
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = { enable = false },
-    },
-  }
-}
+        vim.lsp.config(name, config)
 
+    end
+
+end
