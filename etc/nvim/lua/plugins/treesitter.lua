8a54b5fc7356d5bd9175544a697e300b275f9055
carnivuth <matti200042@gmail.com>
2025-12-18 15:08:50 +0100

    fixed treesitter after breaking update

--- etc/nvim/lua/plugins/treesitter.lua
+++ etc/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })()
-  end,
-
-  main = "nvim-treesitter.configs",
+  lazy = false,
+  build = ':TSUpdate',
   opts = {
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = true,
-    },
-    indent = {
-      enable = true
-    },
-    ensure_installed = { "rust", "bash", "terraform", "python", "dockerfile", "yaml", "jinja","jinja_inline","c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go", "javascript", "html" },
+    require'nvim-treesitter'.install { "rust", "bash", "terraform", "python", "dockerfile", "yaml", "jinja","jinja_inline","c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go", "javascript", "html" },
   }
 }
