a7ceecaca06b8e27961878ca59b5b9f5bbff6f7a
carnivuth <matti200042@gmail.com>
2025-12-19 15:57:34 +0100

    updated telescope version

--- etc/nvim/lua/plugins/telescope.lua
+++ etc/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-  'nvim-telescope/telescope.nvim', branch = '0.1.x',
+  'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
   dependencies = { 'nvim-lua/plenary.nvim' },
 
   config = function()
