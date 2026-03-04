639bb09cdebbfa85c8aea9268d0259b53d8e452e
Matteo Longhi <mlonghi@jmawireless.com>
2026-03-04 12:37:56 +0100

    added filtering on env files

--- etc/nvim/lua/plugins/copilot.lua
+++ etc/nvim/lua/plugins/copilot.lua
@@ -6,6 +6,7 @@ return {
   opts = {
     suggestion = {
       enabled = not vim.g.ai_cmp,
+      trigger_on_accept = true,
 
       auto_trigger = true,
       hide_during_completion = vim.g.ai_cmp,
@@ -22,6 +23,13 @@ return {
       markdown = true,
 
       help = true,
+      sh = function ()
+        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+          -- disable for .env files
+          return false
+        end
+        return true
+      end,
     },
   },
 }
