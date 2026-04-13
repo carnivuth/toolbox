491ff8570d9640fbf144230fa45707fe7170693c
Matteo Longhi <mlonghi@jmawireless.com>
2026-04-13 10:19:57 +0200

    minor improvements

--- bin/git_sync.sh
+++ bin/git_sync.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 function git_sync(){
-  find "$SEARCH_DIR" -type d -name ".git" -prune -print 2>/dev/null | sed 's|/.git$||' | parallel -j $(nproc) 'echo "Syncing {}"; cd {} && git pull'
+  find "$SEARCH_DIR" -type d -name ".git" -prune -print 2>/dev/null | sed 's|/.git$||' | parallel 'echo "Syncing {}"; cd {} && git pull'
 }
 
-if test "$#" == '0' || [[ "$1" == '.' ]]  ;then SEARCH_DIR="$(pwd)"; else SEARCH_DIR="$1"; fi
+set -x
+if test "$#" == '0' || [[ "$1" == '.' ]]  ;then echo "pass dir to search for"; exit 1; else SEARCH_DIR="$1"; fi
 git_sync
